a682b31d6f727749d19e506d3572bbe63c9ae5e6
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-11 17:58:38 -0300

    Add project files.

---
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace JogoDeAdivinhacao.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+    }
+}
