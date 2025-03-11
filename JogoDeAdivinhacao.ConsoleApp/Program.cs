58110afb5e9424aeef4b9b4fbe260d9b2f2010cf
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-11 18:00:43 -0300

    Adicionada validação de acerto e mensagens de feedback

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -18,10 +18,24 @@ namespace JogoDeAdivinhacao.ConsoleApp
                 Console.Write("Digite um número entre 1 e 20: ");
                 int numeroDigitado = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("----------------------------------------");
-                Console.WriteLine($"Você digitou: {numeroDigitado}");
-                Console.WriteLine($"O número secreto era: {numeroSecreto}");
-                Console.WriteLine("----------------------------------------");
+                if (numeroDigitado == numeroSecreto)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Parabéns! Você acertou!");
+                    Console.WriteLine("----------------------------------------");
+                }
+                else if (numeroDigitado > numeroSecreto)
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("O número digitado é maior que o número secreto.");
+                    Console.WriteLine("----------------------------------------");
+                }
+                else
+                {
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("O número digitado é menor que o número secreto.");
+                    Console.WriteLine("----------------------------------------");
+                }
 
                 Console.Write("Deseja continuar? (S/N): ");
                 string opcaoContinuar = Console.ReadLine().ToUpper();
