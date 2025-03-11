f10f68018b83e17a6a43a427bd9baddade318265
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-03-11 17:59:11 -0300

    Adicionada estrutura inicial do jogo

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -6,7 +6,23 @@ namespace JogoDeAdivinhacao.ConsoleApp
     {
         static void Main(string[] args)
         {
-        
+            while (true)
+            {
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("Jogo de Adivinhação");
+                Console.WriteLine("----------------------------------------");
+
+                Console.Write("Digite um número: ");
+                int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"\nVocê digitou: {numeroDigitado}");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if (opcaoContinuar != "S")
+                    break;
+            }
         }
     }
 }
